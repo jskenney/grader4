0ba56bffec57c489d4f19f3f4da41b9d50fe3d64
J Kenney <kenney@moboard.com>
2018-12-31 15:45:52 -0500

    switch to unicode from bytes, remove debugging that was causing errors.

--- check.py
+++ check.py
@@ -12,11 +12,14 @@ sys.dont_write_bytecode = True
 #  cond: 'compiles', 'is exactly', 'whitespace', 'has', 'does not have', 'exists', 'is true', 'is false'
 #        'greater than', 'less than', 'greater or equal', 'less or equal'
 # Check the results and see if the test was successful
+# Returns pass/fail and the value it was tested against.
 def test(stdin, source, sourcefile, cond, outvalue, stdout, stderr, returnval, stime, infinite, debug=True):
 
     # set default check variables
     chk_student = stdout
     chk_testcase = outvalue
+
+    # Assume that a file was created
     created_exists = True
 
     # change inputs based on the ___source___
@@ -26,7 +29,7 @@ def test(stdin, source, sourcefile, cond, outvalue, stdout, stderr, returnval, s
         chk_student = infinite
     elif source == 'Student source code' or source == 'Created File':
         try:
-            chk_student = b''.join(open(sourcefile,'rb').readlines())
+            chk_student = u''.join(open(sourcefile,'r').readlines())
         except:
             created_exists = False
     elif source == 'Run time':
@@ -34,72 +37,61 @@ def test(stdin, source, sourcefile, cond, outvalue, stdout, stderr, returnval, s
 
     # Check for NoneType
     if chk_student is None:
-        chk_student = b''
+        chk_student = u''
     if chk_testcase is None:
-        chk_testcase = b''
+        chk_testcase = u''
 
-    # Convert everything to bytes
-    if not isinstance(chk_student, bytes):
-        chk_student = bytes(chk_student, 'utf-8')
-    if not isinstance(chk_testcase, bytes):
-        chk_testcase = bytes(chk_testcase, 'utf-8')
+    # Create diff results
+    diff_student = chk_student
 
-    # Print debugging information if requested
-    if debug:
-        print ('*********** Test Case Conditions *************')
-        print ('This test case checks to see if the ['+source+'] ['+cond+']')
-        print ('Expected: '+str(chk_testcase))
-        print ('Received: '+str(chk_student))
-        print ('**********************************************')
-        
     # check to see if this is the simple "compiles" case
     if cond == 'compiles':
-        return True
+        return True, diff_student
 
     # if condition is 'whitespace' which ignores whitespace,
     # delete whitespace and then do the comparision as 'is exactly'
     if cond == 'whitespace':
         cond = 'is exactly'
-        chk_student = chk_student.replace(b'\n', b'').replace(b'\t', b'').replace(b' ', b'')
-        chk_testcase = chk_testcase.replace(b'\n', b'').replace(b'\t', b'').replace(b' ', b'')
+        chk_student = chk_student.replace(u'\n', u'').replace(u'\t', u'').replace(u' ', u'')
+        chk_testcase = chk_testcase.replace(u'\n', u'').replace(u'\t', u'').replace(u' ', u'')
 
     # check for equality
     if cond == 'is exactly':
-        return chk_student.strip().replace(b'\r', b'') == chk_testcase.strip().replace(b'\r', b'')
+        return chk_student.strip().replace(u'\r', u'') == chk_testcase.strip().replace(u'\r', u''), diff_student
 
     # check via the various conditions
     if cond == 'has':
-        return chk_student.find(chk_testcase) != -1
+        return chk_student.find(chk_testcase) != -1, diff_student
     if cond == 'does not have':
-        return chk_student.find(chk_testcase) == -1
+        return chk_student.find(chk_testcase) == -1, diff_student
 
     # Verify that results existed
     if cond == 'exists':
         if source == 'Student source code' or source == 'Created File':
-            return created_exists
+            return created_exists, diff_student
         if source == 'Output':
-            return chk_student != b''
+            return chk_student != u'', diff_student
 
     # truth checking
     if cond == 'is true':
-        return chk_student == b'1' or chk_student == True
+        return chk_student == u'1' or chk_student == True, diff_student
     if cond == 'is false':
-        return not chk_student == b'1' or chk_student == True
+        return not chk_student == u'1' or chk_student == True, diff_student
 
     # numeric <, <=, >, >=
     try:
         chk_student = float(chk_student)
         chk_testcase = float(chk_testcase)
     except:
-        return False
+        return False, diff_student
 
     if cond == 'greater than':
-        return chk_student > chk_testcase
+        return chk_student > chk_testcase, diff_student
     if cond == 'less than':
-        return chk_student < chk_testcase
+        return chk_student < chk_testcase, diff_student
     if cond == 'greater or equal':
-        return chk_student >= chk_testcase
+        return chk_student >= chk_testcase, diff_student
     if cond == 'less or equal':
-        return chk_student <= chk_testcase
+        return chk_student <= chk_testcase, diff_student
 
-    return False
+    return False, diff_student
