ebe3a075135446e92324f8501643386716d3567c
J Kenney <kenney@usna.edu>
2019-01-28 10:52:15 -0500

    automatically fix permissions if needed

--- run-in-loop.sh
+++ run-in-loop.sh
@@ -2,6 +2,9 @@
 
 while true
 do
+  echo "Fix Permissions Just In Case"
+  chmod 644 *.py Dockerfile README
+  chmod 755 server.py client.py
   echo "Running Server"
   python3 server.py
   echo "restarting server in 10 seconds"
