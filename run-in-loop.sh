e223e6f71fcd433a5d2511e0827e7d6df6c0daad
J. Kenney <kenney@usna.edu>
2021-09-14 09:44:46 -0400

    do not create caches

--- run-in-loop.sh
+++ run-in-loop.sh
@@ -6,7 +6,7 @@ do
   chmod 644 *.py Dockerfile README
   chmod 755 server.py client.py
   echo "Running Server"
-  python3 server.py
+  python3 -B server.py
   echo "restarting server in 10 seconds"
   sleep 10
 done
