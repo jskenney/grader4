dda276b744b024ca9b46c50028c883036359a799
J. Kenney <kenney@usna.edu>
2021-09-14 11:03:31 -0400

    Run test cases in serial

---
+++ run-in-seq.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo "Fix Permissions Just In Case"
+chmod 644 *.py Dockerfile README
+chmod 755 server.py client.py
+
+while true
+echo "Running Server"
+do
+  docker run --rm $(docker build -q .)
+  echo "restarting server in 5 seconds"
+  sleep 5
+done
