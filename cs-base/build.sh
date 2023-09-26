ad5ea1f5c50936ca2b13e64608665c580f0d84ab
Jeff Kenney <kenney@usna.edu>
2023-09-26 10:46:43 -0400

    create submit user home dir

--- cs-base/build.sh
+++ cs-base/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Remove all existing docker images
+# This is dangerous if there is more than one base
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
+docker system prune -a -f
 
-
+# Build this image
 docker build -t cs-base .
