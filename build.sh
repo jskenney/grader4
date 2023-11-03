99f409149b65b902962bef98a740adc0c8812a7f
Jeff Kenney <kenney@usna.edu>
2023-11-03 10:26:53 -0400

    add prune to build script

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 # Delete all docker containers and images do:
 docker rm $(docker ps -a -q)
 docker rmi $(docker images -q)
+docker system prune -a -f
 
 ###############################################################################
 # Build the environments
